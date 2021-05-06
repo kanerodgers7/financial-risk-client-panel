990c23461e00daf4929754f761714a8be8e6000f
kanerodgers7 <jellyfish9629@gmail.com>
2021-05-06 18:48:14 +0530

    Table.scss and TableDataProcessHelper.js updated as per admin panel

--- src/helpers/TableDataProcessHelper.js
+++ src/helpers/TableDataProcessHelper.js
@@ -5,27 +5,45 @@ import Checkbox from '../common/Checkbox/Checkbox';
 export const processTableDataByType = ({ header, row, actions }) => {
   const { type } = header;
   const currentData = row[`${header.name}`];
-  const { handleDrawerState, handleCheckBoxState } = actions;
+  const { handleDrawerState, handleCheckBoxState, handleViewDocument } = actions;
 
   switch (type) {
     case 'date':
       return moment(currentData).format('DD-MMM-YYYY');
     case 'modal':
-      return (
-        <div className="link" onClick={(e) => {e.stopPropagation();handleDrawerState(header, currentData)}}>
-          {currentData.value}
-        </div>
-      );
+      if (currentData?.value) {
+        return (
+                <div
+                        className="link"
+                        onClick={e => {
+                          e.stopPropagation();
+                          handleDrawerState(header, currentData, row);
+                        }}
+                >
+                  {currentData?.value}
+                </div>
+        );
+      }
+      return '-';
+
     case 'boolean':
       return (
-        <Checkbox
-          title={null}
-          checked={currentData.value}
-          onChange={e => handleCheckBoxState(e.target.checked, header, currentData)}
-        />
+              <div className="table-checkbox">
+                <Checkbox
+                        title={null}
+                        checked={currentData?.value ?? currentData}
+                        onChange={e => handleCheckBoxState(e.target.checked, header, currentData, row)}
+                />
+              </div>
       );
     case 'booleanString':
       return currentData ? 'Yes' : 'No';
+    case 'link':
+      return (
+              <div className="link" onClick={() => handleViewDocument(header, row)}>
+                {currentData}
+              </div>
+      );
 
     default:
       return currentData;
