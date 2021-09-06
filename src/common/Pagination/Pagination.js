2fddb64d827c6309289c31c42f22f89ab815bab2
kanerodgers7 <jellyfish9629@gmail.com>
2021-09-06 15:11:18 +0530

    pagination - first and last page click changes

--- src/common/Pagination/Pagination.js
+++ src/common/Pagination/Pagination.js
@@ -27,8 +27,8 @@ const Pagination = props => {
 
   const onNextClick = () => (page < pages ? pageActionClick(page + 1) : null);
   const onPrevClick = () => (page > 1 ? pageActionClick(page - 1) : null);
-  const onFirstClick = () => pageActionClick(1);
-  const onLastClick = () => pageActionClick(pages);
+  const onFirstClick = () => (page > 1 ? pageActionClick(1) : null);
+  const onLastClick = () => (page < pages ? pageActionClick(pages) : null);
   const onChangeLimit = useCallback(
     e => {
       setRecordLimit(e);
