cab964ace995c0db400efdb75456550b42033004
kanerodgers7 <jellyfish9629@gmail.com>
2021-11-02 16:16:05 +0530

    button - isLoading - prop added

--- src/common/Button/Button.js
+++ src/common/Button/Button.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { isLoading, title, buttonType, className, ...restProps } = props;
+  const { isLoading, isDisabled, title, buttonType, className, children, ...restProps } = props;
   const buttonClass = `button ${buttonType}-button ${className}`;
 
   return (
-    <button type="button" className={buttonClass} {...restProps} disabled={isLoading}>
+    <button type="button" className={buttonClass} {...restProps} disabled={isDisabled || isLoading}>
       <div className={isLoading && 'button-loader'}>{!isLoading && title}</div>
+      {children}
     </button>
   );
 };
 
 Button.propTypes = {
-  title: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
   buttonType: PropTypes.oneOf([
     'primary',
     'secondary',
@@ -22,17 +23,20 @@ Button.propTypes = {
     'primary-1',
     'outlined-primary',
     'outlined-secondary',
-    'outlined-red',
     'outlined-primary-small',
     'outlined-red-small',
+    'outlined-red',
   ]).isRequired,
   className: PropTypes.string,
   isLoading: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  children: PropTypes.element.isRequired,
 };
 
 Button.defaultProps = {
   className: '',
   isLoading: false,
+  isDisabled: false,
 };
 
 export default Button;
