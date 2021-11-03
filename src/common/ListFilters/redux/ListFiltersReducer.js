4d49b64456d508d8a36032c6bb508acdbaf94ac0
kanerodgers7 <jellyfish9629@gmail.com>
2021-11-03 17:27:51 +0530

    list filter - initial state added for application status

--- src/common/ListFilters/redux/ListFiltersReducer.js
+++ src/common/ListFilters/redux/ListFiltersReducer.js
@@ -1,7 +1,7 @@
 import { LOGIN_REDUX_CONSTANTS } from '../../../screens/auth/login/redux/LoginReduxConstants';
 import { LIST_FILTERS_REDUX_CONSTANTS } from './ListFiltersReduxConstants';
 
-export const listFilterReducer = (state = {}, action) => {
+export const listFilterReducer = (state = {applicationListFilters: {status: 'SENT_TO_INSURER,REVIEW_APPLICATION,UNDER_REVIEW,PENDING_INSURER_REVIEW,AWAITING_INFORMATION'}}, action) => {
   switch (action.type) {
     case LIST_FILTERS_REDUX_CONSTANTS.SAVE_APPLIED_FILTERS:
       return {
