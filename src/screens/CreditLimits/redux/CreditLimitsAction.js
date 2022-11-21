9e06c04f4459a7dc0ec22871c903094e28a54c12
kanerodgers7 <jellyfish9629@gmail.com>
2022-11-21 15:28:10 +0530

    fix/n.map issue

--- src/screens/CreditLimits/redux/CreditLimitsAction.js
+++ src/screens/CreditLimits/redux/CreditLimitsAction.js
@@ -159,7 +159,7 @@ export const getCreditLimitsFilterDropDownDataBySearch = options => {
 
       if (response?.data?.status === 'SUCCESS') {
         dispatch({
-          type: CREDIT_LIMITS_FILTER_LIST_REDUX_CONSTANTS.CREDIT_LIMITS_FILTER_LIST,
+          type: CREDIT_LIMITS_FILTER_LIST_REDUX_CONSTANTS.CREDIT_LIMITS_LIST_BY_SEARCH,
           data: response?.data?.data,
           name: options.entityType,
         });
